480644ceb349b417ec10b1541fbb3d9a4f5ff949
martinsookael <martinsookael@gmail.com>
2026-03-02 01:12:32 +0200

    test_more

--- centrale/src/request.rs
+++ centrale/src/request.rs
@@ -13,23 +13,48 @@ pub async fn handle_request(req: HttpRequest) -> impl Responder {
     }
 }
 
-use actix_web::{App, HttpResponse, Responder, test, web};
+use actix_web::{HttpResponse, Responder};
 
 #[actix_rt::test]
 async fn test_empty_host_header() {
     //
+    use actix_web::{App, test, web};
     let app = test::init_service(App::new().route("/", web::get().to(handle_request))).await;
-
-    let req = test::TestRequest::get()
-        .uri("/")
-        //.insert_header(("Host", ""))
-        .to_request();
-
+    let req = test::TestRequest::get().uri("/").to_request();
     let resp = test::call_service(&app, req).await;
 
-    //assert!(resp.status().is_client_error());
+    assert!(resp.status().is_client_error());
     let body = test::read_body(resp).await;
     let expected_body =
         serde_json::to_string(&serde_json::json!({ "error": "Not authenticated" })).unwrap();
     assert_eq!(body, expected_body);
+}
+
+#[actix_rt::test]
+async fn has_referrer_ok() {
+    //
+    use actix_web::{App, test, web};
+    let app = test::init_service(App::new().route("/", web::get().to(handle_request))).await;
+    let req = test::TestRequest::get()
+        .uri("/")
+        .insert_header(("Referer", "Some"))
+        .to_request();
+
+    let resp = test::call_service(&app, req).await;
+    assert!(resp.status().is_success());
+}
+
+#[actix_rt::test]
+async fn has_host_ok() {
+    //
+    use actix_web::{App, test, web};
+    let app = test::init_service(App::new().route("/", web::get().to(handle_request))).await;
+
+    let req = test::TestRequest::get()
+        .uri("/")
+        .insert_header(("Host", "Some"))
+        .to_request();
+
+    let resp = test::call_service(&app, req).await;
+    assert!(resp.status().is_success());
 }
