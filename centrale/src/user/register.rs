37eb750b0ef45cd134f714d44bae35a34f843236
martinsookael <martinsookael@gmail.com>
2026-03-03 21:35:54 +0200

    test for user adding

---
+++ centrale/src/user/register.rs
@@ -0,0 +1,40 @@
+use crate::error::CentraleError;
+use actix_web::web;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct RegisterUser {
+    pub username: String,
+    pub password: String,
+}
+
+pub fn handle_register(json: web::Json<RegisterUser>) -> Result<String, CentraleError> {
+    // Extract the data from the JSON payload
+    let register_request = json.into_inner();
+    let username = register_request.username;
+    let _password = register_request.password;
+    Ok(format!("User {} registered successfully!", username))
+}
+
+#[actix_rt::test]
+async fn post_new_user() {
+    use crate::user::add::add_user;
+    use actix_web::{App, test, web};
+
+    let app = test::init_service(App::new().route("/api/user", web::post().to(add_user))).await;
+    use serde_json::json;
+
+    let payload = json!({
+        "username": "testuser",
+        "password": "testpassword"
+    });
+
+    let req = test::TestRequest::post()
+        .uri("/api/user")
+        .insert_header(("Content-Type", "application/json"))
+        .set_json(&payload)
+        .to_request();
+
+    let resp = test::call_service(&app, req).await;
+    assert!(resp.status().is_success());
+}
