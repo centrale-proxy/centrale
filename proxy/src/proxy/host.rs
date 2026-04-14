025a1a6687b3a74fa7f237b8e9f8663d3d170496
martinsookael <martinsookael@gmail.com>
2026-04-14 13:52:08 +0300

    disable Referer for now for subdomain

--- proxy/src/proxy/host.rs
+++ proxy/src/proxy/host.rs
@@ -4,13 +4,14 @@ use actix_web::HttpRequest;
 pub fn get_host(req: &HttpRequest) -> Result<String, CentraleError> {
     let header = req.headers();
     let host = header.get("Host");
-    let referer = header.get("Referer");
+    // Cloudflare likes Referer sometimes
+    // let referer = header.get("Referer");
     let from_url = req.uri();
 
     if let Some(host) = host {
         Ok(host.to_str()?.to_string())
-    } else if let Some(referer) = referer {
-        Ok(referer.to_str()?.to_string())
+        // } else if let Some(referer) = referer {
+        //    Ok(referer.to_str()?.to_string())
     } else {
         Ok(from_url.to_string())
     }
