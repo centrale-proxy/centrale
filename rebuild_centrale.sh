c695bd4f1d578c2eda48636813c9a7b486b2e5c3
martinsookael <martinsookael@gmail.com>
2026-04-15 15:15:13 +0300

    rebase at rebuild

--- rebuild_centrale.sh
+++ rebuild_centrale.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "/home/www/centrale/proxy"
 git pull
+git rebase
 cd "/home/www/centrale/target/release"
 cargo build --release
 sudo setcap 'cap_net_bind_service=+ep' /home/www/centrale/target/release/proxy
