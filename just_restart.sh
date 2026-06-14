0753011bb1914302860a1fd84aaf784b200a7533
martinsookael <martinsookael@gmail.com>
2026-06-14 14:13:02 +0300

    attempt to reread .env on just_restart

--- just_restart.sh
+++ just_restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo setcap 'cap_net_bind_service=+ep' /home/www/centrale/target/release/proxy
+systemctl daemon-reload
 service centrale stop
 service centrale start
 journalctl -u centrale.service -f
