3da2fad41dedbc681a4409db195268efd3b845de
martinsookael <martinsookael@gmail.com>
2026-06-15 00:26:37 +0300

    rename proxy -> centrale

--- just_restart.sh
+++ just_restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo setcap 'cap_net_bind_service=+ep' /home/www/centrale/target/release/proxy
+sudo setcap 'cap_net_bind_service=+ep' /home/www/centrale/target/release/centrale
 systemctl daemon-reload
 service centrale stop
 service centrale start
--- rebuild_centrale.sh
+++ rebuild_centrale.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd "/home/www/centrale/proxy"
+cd "/home/www/centrale/centrale"
 git pull
 git rebase
 cd "/home/www/centrale/target/release"
 cargo build --release
-sudo setcap 'cap_net_bind_service=+ep' /home/www/centrale/target/release/proxy
+sudo setcap 'cap_net_bind_service=+ep' /home/www/centrale/target/release/centrale
 service centrale stop
 service centrale start
 journalctl -u centrale.service -f
