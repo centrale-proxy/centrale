25d11f513c3d0083547ff8a9b6c7b0a531a367e5
martinsookael <martinsookael@gmail.com>
2026-07-06 02:28:49 +0300

    allow_cors_for_same_domain

--- centrale/src/server/start.rs
+++ centrale/src/server/start.rs
@@ -2,11 +2,43 @@ use crate::proxy::wildcard::create_client::create_client_with_cert;
 use crate::server::rate_limiter::get_rate_limiter_config;
 use crate::server::routes::routes;
 // server::log::log_middleware
+use actix_cors::Cors;
 use actix_governor::Governor;
 use actix_web::{App, HttpServer, web};
 use config::CentraleConfig;
 use dir_and_db_pool::db::DbPool;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+fn origin_is_allowed(origin: &str, base_domain: &str) -> bool {
+    let without_scheme = origin
+        .strip_prefix("https://")
+        .or_else(|| origin.strip_prefix("http://"));
+
+    let Some(without_scheme) = without_scheme else {
+        return false;
+    };
+
+    let host_and_port = without_scheme.split('/').next().unwrap_or_default();
+    let host = host_and_port.split(':').next().unwrap_or_default();
+
+    host == base_domain || host.ends_with(&format!(".{}", base_domain))
+}
+
+fn cors_for_domain(base_domain: &str) -> Cors {
+    let base_domain = base_domain.to_string();
+
+    Cors::default()
+        .allowed_origin_fn(move |origin, _req_head| {
+            origin
+                .to_str()
+                .map(|origin| origin_is_allowed(origin, &base_domain))
+                .unwrap_or(false)
+        })
+        .allow_any_method()
+        .allow_any_header()
+        .supports_credentials()
+        .max_age(3600)
+}
 
 #[actix_web::main]
 pub async fn start_server(db: DbPool) -> std::io::Result<()> {
@@ -21,6 +53,7 @@ pub async fn start_server(db: DbPool) -> std::io::Result<()> {
     let client = create_client_with_cert().unwrap();
 
     let proxy_is_443 = CentraleConfig::get("CENTRALE_IS_443");
+    let cors_domain = CentraleConfig::get("DOMAIN");
 
     if proxy_is_443 == "true".to_string() {
         // SSL //
@@ -29,6 +62,8 @@ pub async fn start_server(db: DbPool) -> std::io::Result<()> {
         builder.set_certificate_chain_file(CentraleConfig::cert_pub_key())?;
         // SERVER ITSELF
         HttpServer::new(move || {
+            let cors = cors_for_domain(&cors_domain);
+
             App::new()
                 .configure(routes)
                 .app_data(web::Data::new(db.clone()))
@@ -39,6 +74,7 @@ pub async fn start_server(db: DbPool) -> std::io::Result<()> {
                 })
                  */
                 .wrap(Governor::new(&governor_conf))
+                .wrap(cors)
                 .app_data(web::Data::new(client.clone()))
         })
         .workers(CentraleConfig::PROXY_SERVER_WORKERS)
@@ -49,6 +85,8 @@ pub async fn start_server(db: DbPool) -> std::io::Result<()> {
         // NOT PORT 443
         // SERVER ITSELF
         HttpServer::new(move || {
+            let cors = cors_for_domain(&cors_domain);
+
             App::new()
                 .configure(routes)
                 .app_data(web::Data::new(db.clone()))
@@ -59,6 +97,7 @@ pub async fn start_server(db: DbPool) -> std::io::Result<()> {
                 })
                 */
                 .wrap(Governor::new(&governor_conf))
+                .wrap(cors)
                 .app_data(web::Data::new(client.clone()))
         })
         .workers(CentraleConfig::PROXY_SERVER_WORKERS)
